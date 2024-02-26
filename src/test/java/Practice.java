8899f1e9627d7a974ceb54ec6f99c79041dd994b
Sid P <s.richa2506@gmail.com>
2024-02-26 23:46:26 +0530

    updated pom and test classes

---
+++ src/test/java/Practice.java
@@ -0,0 +1,33 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+@Test
+public class Practice {
+    @Test
+    public void testName() {
+
+
+
+        System.out.printf("Hello and welcome!");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.globalsqa.com/demo-site/frames-and-windows/#iFrame");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        WebElement frame= driver.findElement(By.xpath("//iframe[@name=\"globalSqa\"]"));
+        driver.switchTo().frame(frame);
+        WebElement list = driver.findElement(By.xpath("//span[@id=\"current_filter\"]"));
+        list.click();
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("arguments[0].innerText='SOFTWARE TESTING'",list );
+
+
+
+    }}
+
